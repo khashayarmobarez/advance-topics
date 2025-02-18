109ed73b1626fe9e4713dd0f2d9a9eb781ac2169
khashayar <khashayarmobarezbusiness@gmail.com>
2025-02-18 19:11:25 +0330

    refactor: remove default document and update home page with dynamic head IT'S NOT WORKING

---
+++ pages/dynamicHeader/index.js
@@ -0,0 +1,31 @@
+import Head from 'next/head';
+
+
+export async function getServerSideProps() {
+    return {
+        props: {
+            data: {
+                name: 'khashayar dynamic train',
+                title: 'ssr page',
+                description: 'a dynamic data to pass to the head'
+            }
+        }
+    };
+}
+
+
+const Index = ({ data }) => {
+    const { name, title, description } = data;
+
+    return (
+        <div>
+            <Head>
+                <title>{title}</title>
+                <meta name="description" content={description} />
+            </Head>
+            <h1>dynamic Head - {name}</h1>
+        </div>
+    );
+};
+
+export default Index;
