192a56e3da46c79032d4c1de88fab7464c472f05
khashayar <khashayarmobarezbusiness@gmail.com>
2025-02-25 19:28:16 +0330

    redirecting in nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // example of redirect
+  async redirects() {
+    return [
+      {
+        source: '/old-path',
+        destination: '/dynamicHeader',
+        // below is for ceo
+        permanent: true,
+      },
+    ];
+  }
 };
 
 export default nextConfig;
